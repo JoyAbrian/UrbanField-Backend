bd94ca5ee00e30196d0f71e632ab5a2f290ab2ab
anisanhy <joyabrianrantepasang14@gmail.com>
2024-06-23 23:40:52 +0800

    fix: fix image not found

--- sql/InsertDummyData.sql
+++ sql/InsertDummyData.sql
@@ -31,26 +31,26 @@ INSERT INTO field_types (name) VALUES
 
 -- Insert dummy data for fields
 INSERT INTO fields (name, type_id, city, address, street_address, image_url, image_url2, image_url3, price_per_hour, opening_time, closing_time) VALUES
-    ('Gor Badminton Pelita', 2, 'Kota Makassar', 'Kecamatan Rappocini', 'Jl. Pelita Raya IV No.32, Balla Parang, Kec. Rappocini, Kota Makassar, Sulawesi Selatan 90222', 'img/field_image/gorbadmintonpelita1.png', 'img/field_image/gorbadmintonpelita2.png', 'img/field_image/gorbadmintonpelita3.png', 30000, '07:00:00', '19:00:00'),
-    ('Laniang Badminton Hall', 2, 'Kota Makassar', 'Kecamatan Tamalanrea', 'Blok AA No. 9, BTP Jl. Laniang, Tamalanrea, Kec. Tamalanrea, Kota Makassar, Sulawesi Selatan 90245', 'img/field_image/laniangbadminton1.png', 'img/field_image/laniangbadminton2.png', 'img/field_image/laniangbadminton3.png', 55000, '09:00:00', '23:00:00'),
-    ('Lapangan Bulutangkis Phinisi', 2, 'Kota Makassar', 'Kecamatan Rappocini', 'Jl. Phinisi No. 1, Balla Parang, Kec. Rappocini, Kota Makassar, Sulawesi Selatan 90222', 'img/field_image/badmintonphinisi1.png', 'img/field_image/badmintonphinisi2.png', 'img/field_image/badmintonphinisi3.png', 40000, '08:00:00', '20:00:00'),
-    ('The Cage Makassar', 2, 'Kota Makassar', 'Kecamatan Panakkukang', 'Jl. Inspeksi Kanal No.69, Karuwisi, Kec. Panakkukang, Kota Makassar, Sulawesi Selatan 90231', 'img/field_image/thecagemakassar1.png', 'img/field_image/thecagemakassar2.png', 'img/field_image/thecagemakassar3.png', 30000, '10:00:00', '23:00:00'),
-    ('Celebes dan gym', 1, 'Kota Makassar', 'Kecamatan Manggala', 'Jl. Tamangapa Raya 3 No.24, Bangkala, Kec. Manggala, Kota Makassar, Sulawesi Selatan 90235', 'img/field_image/futsalgymcelebes1.png', 'img/field_image/futsalgymcelebes2.png', 'img/field_image/futsalgymcelebes3.png', 55000, '07:00:00', '23:00:00'),
-    ('Lapangan GOR MBC Borong', 2, 'Kota Makassar', 'Kecamatan Manggala', 'Jl. Borong Raya Baru, Batua, Kec. Manggala, Kota Makassar, Sulawesi Selatan 90233', 'img/field_image/mbcborong1.png', 'img/field_image/mbcborong2.png', 'img/field_image/mbcborong3.png', 60000, '07:00:00', '23:00:00'),
-    ('Jo & K Badminton', 2, 'Kota Makassar', 'Kecamatan Tamalate', 'Jl. Daeng Tata 1 No.2, Parang Tambung, Kec. Tamalate, Kota Makassar, Sulawesi Selatan 90223', 'img/field_image/jokbadminton1.png', 'img/field_image/jokbadminton2.png', 'img/field_image/jokbadminton3.png', 65000, '08:00:00', '17:00:00'),
-    ('The Futsal NTI Makassar', 1, 'Kota Makassar', 'Kecamatan Tamalanrea', 'Jl. Nusa Tamalanrea Indah, Kapasa, Tamalanrea, Kota Makassar, Sulawesi Selatan 90245, Makassar 90245', 'img/field_image/futsalntimakassar1.png', 'img/field_image/futsalntimakassar2.png', 'img/field_image/futsalntimakassar3.png', 50000, '08:00:00', '23:00:00'),
-    ('Smash Badminton Court', 2, 'Kota Makassar', 'Kecamatan Makassar', 'Jl. S. Saddang II No.16, Maradekaya Sel., Kec. Makassar, Kota Makassar, Sulawesi Selatan 90143', 'img/field_image/smashbadmintoncourt1.png', 'img/field_image/smashbadmintoncourt2.png', 'img/field_image/smashbadmintoncourt3.png', 40000, '08:00:00', '19:00:00'),
-    ('Futsal arena Telkom', 1, 'Kota Makassar', 'Kecamatan Rappocini', 'Jl. A. P. Pettarani No.2, Gn. Sari, Kec. Rappocini, Kota Makassar, Sulawesi Selatan 90221', 'img/field_image/futsalarenatelkom1.png', 'img/field_image/futsalarenatelkom2.png', 'img/field_image/futsalarenatelkom3.png', 54000, '08:00:00', '23:00:00'),
-    ('Futsal Holic', 1, 'Kota Makassar', 'Kecamatan Rappocini', 'Jl. Monginsidi Baru Blok AB 12 No.7, Balla Parang, Kec. Rappocini, Kota Makassar, Sulawesi Selatan 90222', 'img/field_image/futsalholic1.png', 'img/field_image/futsalholic2.png', 'img/field_image/futsalholic3.png', 75000, '08:00:00', '23:00:00'),
-    ('Lapangan Bola Voli Karebosi', 3, 'Kota Makassar', 'Kecamatan Ujung Pandang', 'Jln.kajaolalido no.1, Makassar, Sulawesi Selatan 90000', 'img/field_image/volikarebosi1.png', 'img/field_image/volikarebosi2.png', 'img/field_image/volikarebosi3.png', 75000, '08:00:00', '23:00:00');
+    ('Gor Badminton Pelita', 2, 'Kota Makassar', 'Kecamatan Rappocini', 'Jl. Pelita Raya IV No.32, Balla Parang, Kec. Rappocini, Kota Makassar, Sulawesi Selatan 90222', '/img/field_image/gorbadmintonpelita1.png', '/img/field_image/gorbadmintonpelita2.png', '/img/field_image/gorbadmintonpelita3.png', 30000, '07:00:00', '19:00:00'),
+    ('Laniang Badminton Hall', 2, 'Kota Makassar', 'Kecamatan Tamalanrea', 'Blok AA No. 9, BTP Jl. Laniang, Tamalanrea, Kec. Tamalanrea, Kota Makassar, Sulawesi Selatan 90245', '/img/field_image/laniangbadminton1.png', '/img/field_image/laniangbadminton2.png', '/img/field_image/laniangbadminton3.png', 55000, '09:00:00', '23:00:00'),
+    ('Lapangan Bulutangkis Phinisi', 2, 'Kota Makassar', 'Kecamatan Rappocini', 'Jl. Phinisi No. 1, Balla Parang, Kec. Rappocini, Kota Makassar, Sulawesi Selatan 90222', '/img/field_image/badmintonphinisi1.png', '/img/field_image/badmintonphinisi2.png', '/img/field_image/badmintonphinisi3.png', 40000, '08:00:00', '20:00:00'),
+    ('The Cage Makassar', 2, 'Kota Makassar', 'Kecamatan Panakkukang', 'Jl. Inspeksi Kanal No.69, Karuwisi, Kec. Panakkukang, Kota Makassar, Sulawesi Selatan 90231', '/img/field_image/thecagemakassar1.png', '/img/field_image/thecagemakassar2.png', '/img/field_image/thecagemakassar3.png', 30000, '10:00:00', '23:00:00'),
+    ('Celebes dan gym', 1, 'Kota Makassar', 'Kecamatan Manggala', 'Jl. Tamangapa Raya 3 No.24, Bangkala, Kec. Manggala, Kota Makassar, Sulawesi Selatan 90235', '/img/field_image/futsalgymcelebes1.png', '/img/field_image/futsalgymcelebes2.png', '/img/field_image/futsalgymcelebes3.png', 55000, '07:00:00', '23:00:00'),
+    ('Lapangan GOR MBC Borong', 2, 'Kota Makassar', 'Kecamatan Manggala', 'Jl. Borong Raya Baru, Batua, Kec. Manggala, Kota Makassar, Sulawesi Selatan 90233', '/img/field_image/mbcborong1.png', '/img/field_image/mbcborong2.png', '/img/field_image/mbcborong3.png', 60000, '07:00:00', '23:00:00'),
+    ('Jo & K Badminton', 2, 'Kota Makassar', 'Kecamatan Tamalate', 'Jl. Daeng Tata 1 No.2, Parang Tambung, Kec. Tamalate, Kota Makassar, Sulawesi Selatan 90223', '/img/field_image/jokbadminton1.png', '/img/field_image/jokbadminton2.png', '/img/field_image/jokbadminton3.png', 65000, '08:00:00', '17:00:00'),
+    ('The Futsal NTI Makassar', 1, 'Kota Makassar', 'Kecamatan Tamalanrea', 'Jl. Nusa Tamalanrea Indah, Kapasa, Tamalanrea, Kota Makassar, Sulawesi Selatan 90245, Makassar 90245', '/img/field_image/futsalntimakassar1.png', '/img/field_image/futsalntimakassar2.png', '/img/field_image/futsalntimakassar3.png', 50000, '08:00:00', '23:00:00'),
+    ('Smash Badminton Court', 2, 'Kota Makassar', 'Kecamatan Makassar', 'Jl. S. Saddang II No.16, Maradekaya Sel., Kec. Makassar, Kota Makassar, Sulawesi Selatan 90143', '/img/field_image/smashbadmintoncourt1.png', '/img/field_image/smashbadmintoncourt2.png', '/img/field_image/smashbadmintoncourt3.png', 40000, '08:00:00', '19:00:00'),
+    ('Futsal arena Telkom', 1, 'Kota Makassar', 'Kecamatan Rappocini', 'Jl. A. P. Pettarani No.2, Gn. Sari, Kec. Rappocini, Kota Makassar, Sulawesi Selatan 90221', '/img/field_image/futsalarenatelkom1.png', '/img/field_image/futsalarenatelkom2.png', '/img/field_image/futsalarenatelkom3.png', 54000, '08:00:00', '23:00:00'),
+    ('Futsal Holic', 1, 'Kota Makassar', 'Kecamatan Rappocini', 'Jl. Monginsidi Baru Blok AB 12 No.7, Balla Parang, Kec. Rappocini, Kota Makassar, Sulawesi Selatan 90222', '/img/field_image/futsalholic1.png', '/img/field_image/futsalholic2.png', '/img/field_image/futsalholic3.png', 75000, '08:00:00', '23:00:00'),
+    ('Lapangan Bola Voli Karebosi', 3, 'Kota Makassar', 'Kecamatan Ujung Pandang', 'Jln.kajaolalido no.1, Makassar, Sulawesi Selatan 90000', '/img/field_image/volikarebosi1.png', '/img/field_image/volikarebosi2.png', '/img/field_image/volikarebosi3.png', 75000, '08:00:00', '23:00:00');
 
 -- Insert dummy data for facilities
 INSERT INTO facilities (name, icon) VALUES
-    ('Parkiran', 'img/facility/bikeIcon.png'),
-    ('Musholla', 'img/facility/mushollaIcon.png'),
-    ('Ruang Ganti', 'img/facility/hangerIcon.png'),
-    ('Kantin', 'img/facility/cafeIcon.png'),
-    ('Toilet', 'img/facility/toiletIcon.png');
+    ('Parkiran', 'car'),
+    ('Musholla', 'musholla'),
+    ('Ruang Ganti', 'hanger'),
+    ('Kantin', 'cafe'),
+    ('Toilet', 'toilet');
 
 -- Insert dummy data for field facilities
 INSERT INTO field_facility (field_id, facility_id) VALUES
@@ -96,14 +96,14 @@ INSERT INTO field_review (field_id, user_id, rating, review) VALUES
 
 -- Insert dummy data for payment methods
 INSERT INTO payment_methods (method, icon) VALUES
-    ('BCA', 'img/payment/bca.png'),
-    ('BNI', 'img/payment/bni.png'),
-    ('BRI', 'img/payment/bri.png'),
-    ('GoPay', 'img/payment/gopay.png'),
-    ('OVO', 'img/payment/ovo.png'),
-    ('ShopeePay', 'img/payment/shopeepay.png'),
-    ('DANA', 'img/payment/dana.png'),
-    ('Qris', 'img/payment/qris.png');
+    ('BCA', '/img/payment/bca.png'),
+    ('BNI', '/img/payment/bni.png'),
+    ('BRI', '/img/payment/bri.png'),
+    ('GoPay', '/img/payment/gopay.png'),
+    ('OVO', '/img/payment/ovo.png'),
+    ('ShopeePay', '/img/payment/shopeepay.png'),
+    ('DANA', '/img/payment/dana.png'),
+    ('Qris', '/img/payment/qris.png');
 
 -- Insert dummy data for bookings
 INSERT INTO bookings (user_id, field_id, date, time, payment_method_id) VALUES
