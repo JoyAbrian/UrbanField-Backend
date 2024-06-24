5988d7c0d719db4286a924012f47a2ceb8ba22bc
anisanhy <joyabrianrantepasang14@gmail.com>
2024-06-25 00:07:31 +0800

    add single field get

--- app/routes/fields.py
+++ app/routes/fields.py
@@ -2,8 +2,9 @@ from flask import request, jsonify
 from app import app, db
 from app.models import Field, FieldType
 
-@app.route('/fields', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def manage_fields():
+@app.route('/fields', methods=['POST', 'GET'])
+@app.route('/fields/<int:field_id>', methods=['GET', 'PUT', 'DELETE'])
+def manage_fields(field_id=None):
     if request.method == 'POST':
         data = request.json
         new_field = Field(
@@ -12,43 +13,66 @@ def manage_fields():
             city=data['city'],
             address=data['address'],
             street_address=data.get('street_address'),
-            image_url=data['image_url'],
+            image_url=data.get('image_url'),
             image_url2=data.get('image_url2'),
             image_url3=data.get('image_url3'),
             price_per_hour=data['price_per_hour'],
-            opening_time=data['opening_time'],
-            closing_time=data['closing_time'],
+            opening_time=data.get('opening_time'),
+            closing_time=data.get('closing_time'),
         )
         db.session.add(new_field)
         db.session.commit()
         return jsonify({"message": "Field created successfully"}), 201
     
     elif request.method == 'GET':
-        fields = Field.query.all()
-        fields_list = []
-        for field in fields:
-            venue = FieldType.query.get(field.type_id)
-            field_data = {
-                "id": field.id,
-                "name": field.name,
-                "type_id": field.type_id,
-                "venue": venue.name,
-                "city": field.city,
-                "address": field.address,
-                "street_address": field.street_address,
-                "image_url": field.image_url,
-                "image_url2": field.image_url2,
-                "image_url3": field.image_url3,
-                "price_per_hour": str(field.price_per_hour),
-                "opening_time": field.opening_time.strftime('%H:%M'),
-                "closing_time": field.closing_time.strftime('%H:%M'),
-            }
-            fields_list.append(field_data)
-        return jsonify(fields_list), 200
+        if field_id is not None:
+            field = Field.query.get(field_id)
+            if field:
+                venue = FieldType.query.get(field.type_id)
+                field_data = {
+                    "id": field.id,
+                    "name": field.name,
+                    "type_id": field.type_id,
+                    "venue": venue.name,
+                    "city": field.city,
+                    "address": field.address,
+                    "street_address": field.street_address,
+                    "image_url": field.image_url,
+                    "image_url2": field.image_url2,
+                    "image_url3": field.image_url3,
+                    "price_per_hour": field.price_per_hour,
+                    "opening_time": field.opening_time.strftime('%H:%M') if field.opening_time else None,
+                    "closing_time": field.closing_time.strftime('%H:%M') if field.closing_time else None,
+                }
+                return jsonify(field_data), 200
+            else:
+                return jsonify({"error": "Field not found"}), 404
+        
+        else:
+            fields = Field.query.all()
+            fields_list = []
+            for field in fields:
+                venue = FieldType.query.get(field.type_id)
+                field_data = {
+                    "id": field.id,
+                    "name": field.name,
+                    "type_id": field.type_id,
+                    "venue": venue.name,
+                    "city": field.city,
+                    "address": field.address,
+                    "street_address": field.street_address,
+                    "image_url": field.image_url,
+                    "image_url2": field.image_url2,
+                    "image_url3": field.image_url3,
+                    "price_per_hour": field.price_per_hour,
+                    "opening_time": field.opening_time.strftime('%H:%M') if field.opening_time else None,
+                    "closing_time": field.closing_time.strftime('%H:%M') if field.closing_time else None,
+                }
+                fields_list.append(field_data)
+            return jsonify(fields_list), 200
 
     elif request.method == 'PUT':
         data = request.json
-        field_id = data.get('id')
         field = Field.query.get(field_id)
         if field:
             field.name = data.get('name', field.name)
@@ -68,8 +92,6 @@ def manage_fields():
             return jsonify({"error": "Field not found"}), 404
 
     elif request.method == 'DELETE':
-        data = request.json
-        field_id = data.get('id')
         field = Field.query.get(field_id)
         if field:
             db.session.delete(field)
